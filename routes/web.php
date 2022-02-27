cf79cf604e5ed6e295c48c2bc950e2e3b97fa36f
Shayeque Ahmed <shayequeahmed16@gmail.com>
2022-02-27 14:57:04 +0530

    Routes Created

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('upload-image', [ContactController::class, 'index']);
 Route::post('save', [ContactController::class, 'save']);
